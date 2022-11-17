d13ccd378566e1c3b55abd7e1276d0a371139782
Armuri <nguyenarthur4@gmail.com>
2022-11-17 17:22:22 +0100

    update

---
+++ page-accueil.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Exemples</title>
+    </head>
+    <body>
+        <style>
+             <?php 
+          echo "C'est un script PHP!"; ?> 
+        </style>
+  
+    </body>
+</html>
