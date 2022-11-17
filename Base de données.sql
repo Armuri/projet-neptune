6b4f0b048e63a5d840b6861f61faeed3582a33ed
RonanSaves01 <117528466+RonanSaves01@users.noreply.github.com>
2022-11-17 17:12:04 +0100

    Add files via upload

---
+++ Base de données.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0-1.el9
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : jeu. 17 nov. 2022 à 16:06
+-- Version du serveur : 8.0.30
+-- Version de PHP : 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `Hotel Neptune`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Chambres`
+--
+
+CREATE TABLE `Chambres` (
+  `id` char(30) NOT NULL,
+  `login` text NOT NULL,
+  `Chambres libres` int DEFAULT NULL,
+  `Chambres non-libres` int DEFAULT NULL,
+  `Types de chambre` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
