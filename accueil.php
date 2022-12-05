692d9768830c76486a7e12ba46171aa0385f382f
RonanSaves01 <ronan.saves@epsi.fr>
2022-12-05 15:36:58 +0100

    05/12 15:36

--- accueil.php
+++ accueil.php
@@ -10,7 +10,42 @@
         <h1>Hôtel Neptune</h1>
     </header>
     <?php include('./nav-bar.php'); ?>
-    <p>Vous êtes dans la page d'accueil.</p>
+    
     <link rel="stylesheet" href="./css/index.css">
+    <br>
+        <div class="adresse">
+        <h2>161 Avenue Grassion Cibrand, 34280 Carnon.</h2>
+        </div>  
+        <br>
+        <div class="description">
+            <div class="texte-description">
+                <h2>Bienvenue à l'hôtel Neptune</h2>
+                <h3>L'hôtel Néptune est une somptueuse chambre d'hôte se situant à côté de la mer méditéranée.</h3>
+                <h3>Ici vous pourrez profiter du comfort et de la simplicité de nos chambres.Ainsi que de l'accueil et le professionalisme de notre personnel</h3>
+                <br>
+                <br>
+                <h3>la plage se situe à 5 minutes de notre établissement</h3>
+            </div>
+            <div class="image-plage">
+                <img src="plage-carnon.jpg" height="300" width="400">
+            </div>
+        </div>
+        <br>
+        <div class="hotel-description">
+            <div class="texte-hotel">
+                <h2>Notre hôtel</h2>
+                <h3>L'hôtel Néptune de 1 étage, composée de 10 chambres atypiques,</h3>
+                <h3>un accueil et une salle à manger où vous pourrez y prendre le petit-déjeuner le matin</h3>
+            </div>
+            <div class="image-description">
+                <img src="couloir-hotel.jpg" height="300" width="400">
+            </div> 
+        </div>
+    </body>
+        <footer>
+            <div class="icônes">
+                
+            </div>
+        </footer>
 </body>
 </html>
