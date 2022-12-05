05b966264c1c884f743ca6775f671ef8bf185746
RonanSaves01 <ronan.saves@epsi.fr>
2022-12-05 17:26:17 +0100

    05/12 17:16

--- accueil.php
+++ accueil.php
@@ -19,32 +19,50 @@
         <br>
         <div class="description">
             <div class="texte-description">
-                <h2>Bienvenue à l'hôtel Neptune</h2>
+                <u><h2>Bienvenue à l'hôtel Neptune</h2></u>
                 <h3>L'hôtel Néptune est une somptueuse chambre d'hôte se situant à côté de la mer méditéranée.</h3>
-                <h3>Ici vous pourrez profiter du comfort et de la simplicité de nos chambres.Ainsi que de l'accueil et le professionalisme de notre personnel</h3>
+                <h3>Ici vous pourrez profiter du comfort et de la simplicité de nos chambres.Ainsi que de l'accueil et le professionalisme de notre personnel.</h3>
                 <br>
-                <br>
-                <h3>la plage se situe à 5 minutes de notre établissement</h3>
+                <h3>la plage se situe à 5 minutes de notre établissement.</h3>
             </div>
             <div class="image-plage">
-                <img src="plage-carnon.jpg" height="300" width="400">
+                <img src="Images/plage-carnon.jpg" height="300" width="400">
             </div>
         </div>
         <br>
         <div class="hotel-description">
             <div class="texte-hotel">
-                <h2>Notre hôtel</h2>
+                <u><h2>Notre hôtel:</h2></u>
                 <h3>L'hôtel Néptune de 1 étage, composée de 10 chambres atypiques,</h3>
                 <h3>un accueil et une salle à manger où vous pourrez y prendre le petit-déjeuner le matin</h3>
             </div>
             <div class="image-description">
-                <img src="couloir-hotel.jpg" height="300" width="400">
+                <img src="Images/couloir-hotel.jpg" height="300" width="400">
             </div> 
+        </div>
+        <br>
+        <div class="tourisme-description">
+            <div class="texte-tourisme">
+                <u><h2>Ce qu'il faut voir à Carnon:</h2></u>
+                <h3>Vous ne savez pas quoi faire à Carnon?</h3>
+                <h3>Voici quelques lieux emblématiques que vous pouvez visiter:</h3>
+                <ol type="1">
+                    <li>La plage du Petit travers</li>
+                    <li>Eglise Saint Jean du Malte</li>
+                    <li>Le Port de Carnon</li>
+                    <li>...</li>
+                </ol>
+                <h3>D'autres lieux et activités à découvrir peut-être vu</h3>
+                <h3>sur le site du <a href="https://www.petitfute.com/v53805-carnon-34280/">Petit Futé</a></h3>
+            </div>
+            <div class="images-tourismes">
+               <img src="Images/plagedupetittravers.png" alt="Plage du petit travers" height="180" width="280">
+                <img src="Images/eglise-saint-jean-du-malte.jpg" alt="Eglise Saint-Jean du Malte" height="180" width="280">
+                <img src="Images/port-de-carnon.png" alt="Port de carnon" height="180" width="280">
         </div>
     </body>
         <footer>
-            <div class="icônes">
-                
+            <div class="icônes">    
             </div>
         </footer>
 </body>
