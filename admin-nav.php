30b144ba3be378aa0c5a74252874f9f10cf56de2
RonanSaves01 <ronan.saves@epsi.fr>
2022-12-10 17:23:58 +0100

    10/12 17:23

---
+++ admin-nav.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administration - Page d'accueil</title>
+</head>
+<body>
+    <link rel="stylesheet" href="./css/admin-nav.css">
+    <div class="admin-menu">
+        <nav>
+        <ul>
+            <li><a href="">Liste des chambres</a></li>
+            <li><a href="">Liste des réservations</a></li>
+            <li><a href="">Liste des utilisateurs</a></li>
+            <li><a href="http://localhost/phpmyadmin/">Base de données</a></li>
+        </ul>
+        </nav>
+    </div>
+</body>
+</html>
