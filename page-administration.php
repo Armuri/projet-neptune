ab1249702a9c844b0b29b7d14c4505a8106f660d
RonanSaves01 <ronan.saves@epsi.fr>
2022-12-15 23:35:38 +0100

    15/12 23:35

--- page-administration.php
+++ page-administration.php
@@ -11,67 +11,69 @@
     <h1>Page Administration</h1>
     <div class="section_chambres">
         <div class="img-chambre">
-            <img src="./Images/chambres/chambre-double-classique.jpg" alt="chambre-double-classique" height="180" width="280">
+            <img src="./chambres/chambres/chambre-double-classique.jpg" alt="chambre-double-classique" height="180" width="280">
             <div class="chambre-description">
                 <h2>Chambre Double Classique</h2>
             </div>
             <input type="checkbox" name="checkbox">
         </div>
         <div class="chambre">
-            <img src="./Images/chambres/chambre-double-confort.jpg" alt="chambre-double-confort" height="180" width="280">
+            <img src="./chambres/chambres/chambre-double-confort.jpg" alt="chambre-double-confort" height="180" width="280">
             <div class="chambre-description">
                 <h2>Chambre Double Classique</h2>
             </div>
             <input type="checkbox" name="checkbox">
         </div>
         <div class="chambre">
-            <img src="./Images/chambres/chambre-double-deluxe.jpg" alt="chambre-double-deluxe" height="180" width="280">
+            <img src="./chambres/chambres/chambre-double-deluxe.jpg" alt="chambre-double-deluxe" height="180" width="280">
             <div class="chambre-description">
                 <h2>Chambre Double Deluxe</h2>
             </div>
             <input type="checkbox" name="checkbox">
         </div>
+        </div>
         <div class="chambre">
-            <img src="./Images/chambres/chambre-double-economique.jpg" alt="chambre-double-economique" height="180" width="280">
+            <img src="./chambres/chambres/chambre-double-economique.jpg" alt="chambre-double-economique" height="180" width="280">
             <div class="chambre-description">
                 <h2>Chambre Double Economique</h2>
             </div>
             <input type="checkbox" name="checkbox">
         </div>
         <div class="chambre">
-            <img src="./Images/chambres/chambre-lits-jumeaux-classique.jpg" alt="chambre-double-confort" height="180" width="280">
+            <img src="./chambres/chambres/chambre-lits-jumeaux-classique.jpg" alt="chambre-double-confort" height="180" width="280">
             <div class="chambre-description">
                 <h2>Chambre Lits Jumeaux Classique</h2>
             </div>
             <input type="checkbox" name="checkbox">
         </div>
         <div class="chambre">
-            <img src="./Images/chambres/chambre-simple-deluxe.jpg" alt="chambre-simple-deluxe" height="180" width="280">
+            <img src="./chambres/chambres/chambre-simple-deluxe.jpg" alt="chambre-simple-deluxe" height="180" width="280">
             <div class="chambre-description">
                 <h2>Chambre Simple Deluxe</h2>
             </div>
             <input type="checkbox" name="checkbox">
         </div>
         <div class="chambre">
-            <img src="./Images/chambres/chambre-simple-economique.jpg" alt="chambre-simple-economique" height="180" width="280">
+            <img src="./chambres/chambres/chambre-simple-economique.jpg" alt="chambre-simple-economique" height="180" width="280">
             <div class="chambre-description">
                 <h2>Chambre Simple Economique</h2>
             </div>
             <input type="checkbox" name="checkbox">
         </div>
         <div class="chambre">
-            <img src="./Images/chambres/chambre-triple-classique.jpg" alt="chambre-triple-classique" height="180" width="280">
+            <img src="./chambres/chambres/chambre-triple-classique.jpg" alt="chambre-triple-classique" height="180" width="280">
             <div class="chambre-description">
                 <h2>Chambre Triple Classique</h2>
             </div>
             <input type="checkbox" name="checkbox">
         </div>
         <div class="chambre">
-            <img src="./Images/chambres/chambre-triple-economique.jpg" alt="chambre-triple-economique" height="180" width="280">
+            <img src="./chambres/chambres/chambre-triple-economique.jpg" alt="chambre-triple-economique" height="180" width="280">
             <div class="chambre-description">
                 <h2>Chambre Triple Classique</h2>
             </div>
             <input type="checkbox" name="checkbox">
+        </div>
         </div>
     </div>
     <button type="button" form="ajouter">Ajouter une chambre</button>
