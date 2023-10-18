5d7f442bd79180cc47c652097bd82699faf1d0e8
Armuri <nguyenarthur4@gmail.com>
2023-10-18 16:42:16 +0200

    Update inscription.php

--- ba/inscription.php
+++ ba/inscription.php
@@ -37,7 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $insert = $dbh->prepare("INSERT INTO users (nom, prenom, mail, nom_utilisateur, mot_de_passe) VALUES ('$nom', '$prenom','$email','$login','$password')");
     $insert->execute(array('nom' => $nom, 'prenom' => $prenom, 'mail' => $email, 'nom_utilisateur' => $login, 'mot_de_passe' => $password ));
     header('location: ./connexion.php');
-    exit;
+    exit();
 }
 
 ?>
